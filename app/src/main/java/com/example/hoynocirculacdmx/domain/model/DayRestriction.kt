1567c7e8f8f3f74ad3881907d74686d9721be492
claudio.castillo <claudioch.talentoenti@gmail.com>
2026-01-13 22:38:54 -0600

    Add domain models for Hoy No Circula rules

--- app/src/main/java/com/example/hoynocirculacdmx/domain/model/DayRestriction.kt
+++ app/src/main/java/com/example/hoynocirculacdmx/domain/model/DayRestriction.kt
@@ -1,2 +1,15 @@
 package com.example.hoynocirculacdmx.domain.model
 
+/**
+ *  Representa la restricción de circulacion para un día específico.
+ *
+ * @property dayName Nombre del día (ej. Lunes)
+ * @property stickerColor Color del engomado correspondiente
+ * @property restrictedPlates Lista de terminaciones de placa que NO circulan
+ */
+
+data class DayRestriction(
+    val dayName : String,
+    val stickerColor: StickerColor,
+    val restrictedPlates: List<Int>
+)
--- app/src/main/java/com/example/hoynocirculacdmx/domain/model/StickerColor.kt
+++ app/src/main/java/com/example/hoynocirculacdmx/domain/model/StickerColor.kt
@@ -1,2 +1,16 @@
 package com.example.hoynocirculacdmx.domain.model
 
+/**
+ * Representa el color del engomado del programa Hoy No Circula
+ *
+ * Es parte del dominio porque forma parte de las reglas del negocio
+ * y no depende de la UI nide Android
+ */
+
+enum class StickerColor{
+    AMARILLO,
+    ROSA,
+    ROJO,
+    VERDE,
+    AZUL
+}
